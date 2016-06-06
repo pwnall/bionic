31a7b047c19db4e3993aa0a9f08193c0c34c867d
Staphany Park <stapark008@gmail.com>
2016-06-06 00:31:46 -0700

    Add more binders.

--- src/builtin_binders.ts
+++ src/builtin_binders.ts
@@ -10,10 +10,47 @@ export namespace BuiltinBinders {
   };
   builder.binders.add("html", html);
 
+  /** Replaces the element's children with the given text content. */
   export const text: Binder = (target, value) => {
     target.textContent = value;
   };
   builder.binders.add("text", text);
+
+  /**
+   * Shows the element if the value is truthy, hides it otherwise.
+   *
+   * This method sets an inline style attribute for targets of type HTMLElement
+   * only, not SVGElement. The recommended way of showing/hiding elements,
+   * however, is to use class attributes.
+   */
+  export const show: Binder = (target, value) => {
+    (target as HTMLElement).style.display = value ? "" : "none";
+  };
+  builder.binders.add("show", show);
+
+  /**
+   * Hides the element if the value is truthy, shows it otherwise.
+   *
+   * This method sets an inline style attribute for targets of type HTMLElement
+   * only, not SVGElement. The recommended way of showing/hiding elements,
+   * however, is to use class attributes.
+   */
+  export const hide: Binder = (target, value) => {
+    (target as HTMLElement).style.display = value ? "none" : "";
+  };
+  builder.binders.add("hide", hide);
+
+  /** Enables the element if the value is truthy, disables it otherwise. */
+  export const enabled: Binder = (target, value) => {
+    (target as HTMLInputElement).disabled = !value;
+  };
+  builder.binders.add("enabled", enabled);
+
+  /** Disables the element if the value is truthy, enables it otherwise. */
+  export const disabled: Binder = (target, value) => {
+    (target as HTMLInputElement).disabled = !!value;
+  };
+  builder.binders.add("disabled", disabled);
 
   export const attributeFactory: BinderFactory = (suffix) => {
     return (target, value) => {
--- test/builtin_binders_test.ts
+++ test/builtin_binders_test.ts
@@ -5,20 +5,109 @@ import { builder } from "../src/builder";
 import { BuiltinBinders } from "../src/builtin_binders";
 
 describe("BuiltinBinders", () => {
-  let target: Element;
+  let div: Element;
+  let input: HTMLInputElement;
   beforeEach(() => {
-    target = document.createElement("div");
+    div = document.createElement("div");
+    input = document.createElement("input");
+    input.setAttribute("type", "text");
   });
 
   describe(".html", () => {
     it("sets the target's innerHTML", () => {
-      console.log(BuiltinBinders);
-      BuiltinBinders.html(target, "Hello world!");
-      expect(target.innerHTML).to.equal("Hello world!");
+      BuiltinBinders.html(div, "Hello world!");
+      expect(div.innerHTML).to.equal("Hello world!");
     });
 
     it("is registered with the default builder", () => {
       expect(builder.binders.resolve("html")).to.equal(BuiltinBinders.html);
+    });
+  });
+
+  describe(".text", () => {
+    it("sets the target's text content", () => {
+      BuiltinBinders.html(div, "Hello world!");
+      expect(div.textContent).to.equal("Hello world!");
+    });
+
+    it("is registered with the default builder", () => {
+      expect(builder.binders.resolve("text")).to.equal(BuiltinBinders.text);
+    });
+  });
+
+  describe(".show", () => {
+    it("clears the target's 'display' property if the value is true", () => {
+      BuiltinBinders.show(div, true);
+      expect(div.getAttribute("style")).to.equal(null);
+    });
+
+    it("sets the target to 'display: none' if the value is false", () => {
+      BuiltinBinders.show(div, false);
+      expect(div.getAttribute("style")).to.equal("display: none;");
+    });
+
+    it("is registered with the default builder", () => {
+      expect(builder.binders.resolve("show")).to.equal(BuiltinBinders.show);
+    });
+  });
+
+  describe(".hide", () => {
+    it("hides the target if the value is truthy", () => {
+      BuiltinBinders.hide(div, true);
+      expect(div.getAttribute("style")).to.equal("display: none;");
+    });
+
+    it("shows the target if the value is falsey", () => {
+      BuiltinBinders.hide(div, false);
+      expect(div.hasAttribute("style")).to.equal(false);
+    });
+
+    it("is registered with the default builder", () => {
+      expect(builder.binders.resolve("hide")).to.equal(BuiltinBinders.hide);
+    });
+  });
+
+  describe(".enabled", () => {
+    it("enables the target if the value is truthy", () => {
+      input.setAttribute("disabled", "disabled");
+      expect(input.hasAttribute("disabled")).to.equal(true);
+
+      BuiltinBinders.enabled(input, true);
+      expect(input.disabled).to.equal(false);
+      expect(input.hasAttribute("disabled")).to.equal(false);
+    });
+
+    it("disables the target if the value is falsey", () => {
+      BuiltinBinders.enabled(input, false);
+      expect(input.disabled).to.equal(true);
+      expect(input.hasAttribute("disabled")).to.equal(true);
+    });
+
+    it("is registered with the default builder", () => {
+      expect(builder.binders.resolve("enabled")).to.equal(
+          BuiltinBinders.enabled);
+    });
+  });
+
+  describe(".disabled", () => {
+    it("disables the target if the value is truthy", () => {
+      BuiltinBinders.disabled(input, true);
+      expect(input.disabled).to.equal(true);
+      expect(input.hasAttribute("disabled")).to.equal(true);
+    });
+
+    it("enables the target if the value is falsey", () => {
+      input.setAttribute("disabled", "disabled");
+      expect(input.hasAttribute("disabled")).to.equal(true);
+
+      BuiltinBinders.disabled(input, false);
+      expect(input.disabled).to.equal(false);
+      expect(input.hasAttribute("disabled")).to.equal(false);
+    });
+
+    it("is registered with the default builder", () => {
+      expect(builder.binders.resolve("disabled")).to.equal(
+          BuiltinBinders.disabled);
     });
   });
 
@@ -29,19 +118,19 @@ describe("BuiltinBinders", () => {
     });
 
     it("sets the desired attribute to a string value", () => {
-      fooBinder(target, "bar");
-      expect(target.getAttribute("foo")).to.equal("bar");
+      fooBinder(div, "bar");
+      expect(div.getAttribute("foo")).to.equal("bar");
     });
 
     it("sets the attribute correctly when true", () => {
-      fooBinder(target, true);
-      expect(target.getAttribute("foo")).to.equal("foo");
+      fooBinder(div, true);
+      expect(div.getAttribute("foo")).to.equal("foo");
     });
 
     it("removes the attribute when false", () => {
-      target.setAttribute("foo", "bar");
-      fooBinder(target, false);
-      expect(target.hasAttribute("foo")).to.equal(false);
+      div.setAttribute("foo", "bar");
+      fooBinder(div, false);
+      expect(div.hasAttribute("foo")).to.equal(false);
     });
   });
 });
